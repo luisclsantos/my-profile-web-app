5aa3d33ed5d943387c24bc8b3a008036506b8b04
luiscarlima <luiscarlos_lima@outlook.com.br>
2022-12-25 22:58:54 -0300

    app name

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -10,7 +10,7 @@ class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
-      title: 'Flutter Slidy',
+      title: 'Luis Santos',
       theme: ThemeData.dark().copyWith(
         primaryColor: kPrimaryColor,
         scaffoldBackgroundColor: kBackGroundColor,
